dc3984c94be60efdeb064c6da8455c7543f14367
JusMacc <mcabrera1103@gmail.com>
2024-05-08 20:48:18 -0500

    added code

--- Assignments/P01/main.cpp
+++
@@ -1,9 +0,0 @@
-//Misael Cabrera
-
-#include <iostream> // Include the I/O library
-// The main function - the starting point of the program
-int main() {
-    // std::cout is used for output. The << operator is used to send the data to cout.
-    std::cout << "Hello, World!" << std::endl; // Prints "Hello, World!" followed by a new line
-    return 0; // Return 0 to indicate success
-}
